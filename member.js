3ea918e6fc9df81c85e13eb4b2efb269e09916f4
royalliege <royalliege@hotmail.com>
2024-02-10 14:35:05 +0100

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  return {
+    id: 'member',
+    name: 'Member',
+    description: 'Member',
+    skills: [
+      'member-skill-1',
+      'member-skill-2',
+      'member-skill-3'
+    ]
+  };
+}
